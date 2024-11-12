15b9957de954550566871f7623316191ed84e399
VoriMorsi <vorieos@gmail.com>
2024-11-11 18:48:50 -0800

    Initial commit 1.1

--- app/src/test/java/com/example/cs544sortingapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/cs544sortingapplication/ExampleUnitTest.kt
@@ -1,17 +1,21 @@
-package com.example.cs544sortingapplication
-
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+@RunWith(AndroidJUnit4::class)
+class InputBoxUITest {
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-class ExampleUnitTest {
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun inputBox_ShouldContainText_WhenTextIsEntered() {
+        // Assuming the input box has ID `input_box`
+        onView(withId(R.id.input_box)).check(matches(withText("Expected Text")))
     }
 }
