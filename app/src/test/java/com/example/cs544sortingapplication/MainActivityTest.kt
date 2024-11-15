2b2744131489c409b4bbcd304793e644d3be72f4
VoriMorsi <vorieos@gmail.com>
2024-11-14 17:29:36 -0800

    Updated for Junit Test

---
+++ app/src/test/java/com/example/cs544sortingapplication/MainActivityTest.kt
@@ -0,0 +1,14 @@
+package com.example.cs544sortingapplication
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainActivityTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun getArrayFromString() {
+    }
+}
---
+++ app/src/test/java/com/example/cs544sortingapplication/MainActivityTest2.kt
@@ -0,0 +1,12 @@
+package com.example.cs544sortingapplication
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+class MainActivityTest2 {
+
+    @Test
+    fun intermediateInsertionSort() {
+    }
+}
