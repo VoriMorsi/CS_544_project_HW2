2c8372ee494c2ae346a9dbdab8ea7fee64212094
Fracture17 <johnotters@yahoo.com>
2024-11-15 16:55:15 -0800

    Creates basic unit tests and removes old ones

--- app/src/test/java/com/example/cs544sortingapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/cs544sortingapplication/ExampleUnitTest.kt
@@ -1,21 +1,19 @@
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
-import org.junit.Rule
+package com.example.cs544sortingapplication
+
 import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.Assert.*
 
-@RunWith(AndroidJUnit4::class)
-class InputBoxUITest {
 
-    @get:Rule
-    val activityRule = ActivityTestRule(MainActivity::class.java)
+class ExampleUnitTest {
+    @Test
+    fun test_getArrayFromString_Simple() {
+        val x = MainActivity.getArrayFromString("1 2 3")
+        assertArrayEquals(intArrayOf(1, 2, 3), x.array)
+    }
 
     @Test
-    fun inputBox_ShouldContainText_WhenTextIsEntered() {
-        // Assuming the input box has ID `input_box`
-        onView(withId(R.id.input_box)).check(matches(withText("Expected Text")))
+    fun test_intermediateInsertionSort_Simple() {
+        val x = MainActivity.intermediateInsertionSort(intArrayOf(3, 2, 1), 1)
+        assertArrayEquals(intArrayOf(2, 3, 1), x)
     }
 }
