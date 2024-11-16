fd598e8e0b740183f2d7f5d08ce135888619a8ff
Fracture17 <johnotters@yahoo.com>
2024-11-15 16:54:38 -0800

    Update MainActivity.kt
    Refactors intermediateInsertionSort and getArrayFromString into static functions

--- app/src/main/java/com/example/cs544sortingapplication/MainActivity.kt
+++ app/src/main/java/com/example/cs544sortingapplication/MainActivity.kt
@@ -10,14 +10,6 @@ import android.widget.TextView
 import kotlin.system.exitProcess
 
 class MainActivity : ComponentActivity() {
-    final val MIN_ELEMENTS = 3
-    final val MAX_ELEMENTS = 8
-
-    final val ARRAY_TO_SMALL_ERROR_TEXT = "Please enter at least 3 values"
-    final val ARRAY_TO_LARGE_ERROR_TEXT = "Please enter at most 8 values"
-    final val INVALID_ELEMENT_ERROR_TEXT = "Please only enter integers between 0 and 9"
-    final val VALID_ARRAY_TEXT = "Valid array"
-
     data class InputArray(val array: IntArray?, val errorMessage: String)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +29,7 @@ class MainActivity : ComponentActivity() {
 
         submitButton.setOnClickListener {
             val inputArray = getArrayFromString(inputEditText.text.toString())
-            if(inputArray.array == null) {
+            if (inputArray.array == null) {
                 outputText.text = inputArray.errorMessage
             } else {
                 val handler = Handler(mainLooper)
@@ -54,37 +46,47 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    fun getArrayFromString(input: String): InputArray {
-        val trimmedInput = input.trim()
-        val inputArray = trimmedInput.split(" ").filter { it.isNotEmpty() }
+    companion object {
+        final val MIN_ELEMENTS = 3
+        final val MAX_ELEMENTS = 8
 
-        if(!inputArray.all { it.length == 1 && it[0] in '0'..'9' }) {
-            return InputArray(null, INVALID_ELEMENT_ERROR_TEXT)
-        }
+        public final val ARRAY_TOO_SMALL_ERROR_TEXT = "Please enter at least 3 values"
+        final val ARRAY_TOO_LARGE_ERROR_TEXT = "Please enter at most 8 values"
+        final val INVALID_ELEMENT_ERROR_TEXT = "Please only enter integers between 0 and 9"
+        final val VALID_ARRAY_TEXT = "Valid array"
 
-        if(inputArray.size < MIN_ELEMENTS) {
-            return InputArray(null, ARRAY_TO_SMALL_ERROR_TEXT);
-        }
+        fun getArrayFromString(input: String): InputArray {
+            val trimmedInput = input.trim()
+            val inputArray = trimmedInput.split(" ").filter { it.isNotEmpty() }
 
-        if(inputArray.size > MAX_ELEMENTS) {
-            return InputArray(null, ARRAY_TO_LARGE_ERROR_TEXT);
-        }
-
-        val numbers = inputArray.map { it.toInt() }.toMutableList()
-        return InputArray(numbers.toIntArray(), VALID_ARRAY_TEXT)
-    }
-
-    fun intermediateInsertionSort(numbers: IntArray, i: Int): IntArray {
-        val intermediateResult = numbers.copyOf()
-
-        for(j in i downTo 1) {
-            if(intermediateResult[j] < intermediateResult[j - 1]) {
-                val temp = intermediateResult[j]
-                intermediateResult[j] = intermediateResult[j - 1]
-                intermediateResult[j - 1] = temp
+            if (!inputArray.all { it.length == 1 && it[0] in '0'..'9' }) {
+                return InputArray(null, INVALID_ELEMENT_ERROR_TEXT)
             }
+
+            if (inputArray.size < MIN_ELEMENTS) {
+                return InputArray(null, ARRAY_TOO_SMALL_ERROR_TEXT);
+            }
+
+            if (inputArray.size > MAX_ELEMENTS) {
+                return InputArray(null, ARRAY_TOO_LARGE_ERROR_TEXT);
+            }
+
+            val numbers = inputArray.map { it.toInt() }.toMutableList()
+            return InputArray(numbers.toIntArray(), VALID_ARRAY_TEXT)
         }
 
-        return intermediateResult
+        fun intermediateInsertionSort(numbers: IntArray, i: Int): IntArray {
+            val intermediateResult = numbers.copyOf()
+
+            for (j in i downTo 1) {
+                if (intermediateResult[j] < intermediateResult[j - 1]) {
+                    val temp = intermediateResult[j]
+                    intermediateResult[j] = intermediateResult[j - 1]
+                    intermediateResult[j - 1] = temp
+                }
+            }
+
+            return intermediateResult
+        }
     }
 }
